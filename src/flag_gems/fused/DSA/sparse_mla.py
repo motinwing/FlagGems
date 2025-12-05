cbe1211098d80b15b6a26231245001d1cb1d7488
张智晖 <85692926+motinwing@users.noreply.github.com>
2025-12-05 20:55:30 +0800

    Update sparse_mla.py
    Signed-off-by: 张智晖 <85692926+motinwing@users.noreply.github.com>

--- src/flag_gems/fused/DSA/sparse_mla.py
+++ src/flag_gems/fused/DSA/sparse_mla.py
@@ -4,7 +4,7 @@ import triton.language as tl
 
 spar_mla_fwd_configs = [
     triton.Config({"num_stages": 4, "num_warps": 8}),
-    triton.Config({'num_stages': 2, 'num_warps': 4}),
+    triton.Config({"num_stages": 2, "num_warps": 4}),
 ]
 
 
